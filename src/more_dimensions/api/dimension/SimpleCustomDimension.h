1fdc0692ea6938e2fdaa2bcdcbdf957aaf9333ac
Qiuzhizhe <42761326+quizhizhe@users.noreply.github.com>
2024-04-27 02:26:51 -0700

    fix: fix SimpleCustomDimension not export

--- src/more_dimensions/api/dimension/SimpleCustomDimension.h
+++ src/more_dimensions/api/dimension/SimpleCustomDimension.h
@@ -5,6 +5,7 @@
 #include "mc/world/level/dimension/OverworldBrightnessRamp.h"
 #include "mc/world/level/levelgen/structure/StructureFeatureRegistry.h"
 #include "more_dimensions/api/dimension/CustomDimensionManager.h"
+#include "more_dimensions/core/Macros.h"
 
 class BaseGameVersion;
 class Experiments;
@@ -35,31 +36,31 @@ class SimpleCustomDimension : public Dimension {
     GeneratorType generatorType;
 
 public:
-    SimpleCustomDimension(std::string const& name, DimensionFactoryInfo const& info);
+    MORE_DIMENSIONS_API SimpleCustomDimension(std::string const& name, DimensionFactoryInfo const& info);
 
-    static CompoundTag generateNewData(uint seed = 123, GeneratorType generatorType = GeneratorType::Overworld);
+    MORE_DIMENSIONS_API static CompoundTag generateNewData(uint seed = 123, GeneratorType generatorType = GeneratorType::Overworld);
 
-    void init(br::worldgen::StructureSetRegistry const&) override;
+    MORE_DIMENSIONS_API void init(br::worldgen::StructureSetRegistry const&) override;
 
-    std::unique_ptr<WorldGenerator> createGenerator(br::worldgen::StructureSetRegistry const&) override;
+    MORE_DIMENSIONS_API std::unique_ptr<WorldGenerator> createGenerator(br::worldgen::StructureSetRegistry const&) override;
 
-    void upgradeLevelChunk(ChunkSource& chunkSource, LevelChunk& oldLc, LevelChunk& newLc) override;
+    MORE_DIMENSIONS_API void upgradeLevelChunk(ChunkSource& chunkSource, LevelChunk& oldLc, LevelChunk& newLc) override;
 
-    void fixWallChunk(ChunkSource& cs, LevelChunk& lc) override;
+    MORE_DIMENSIONS_API void fixWallChunk(ChunkSource& cs, LevelChunk& lc) override;
 
-    bool levelChunkNeedsUpgrade(LevelChunk const& lc) const override;
+    MORE_DIMENSIONS_API bool levelChunkNeedsUpgrade(LevelChunk const& lc) const override;
 
-    void _upgradeOldLimboEntity(CompoundTag& tag, ::LimboEntitiesVersion vers) override;
+    MORE_DIMENSIONS_API void _upgradeOldLimboEntity(CompoundTag& tag, ::LimboEntitiesVersion vers) override;
 
-    Vec3 translatePosAcrossDimension(Vec3 const& pos, DimensionType did) const override;
+    MORE_DIMENSIONS_API Vec3 translatePosAcrossDimension(Vec3 const& pos, DimensionType did) const override;
 
-    std::unique_ptr<ChunkSource>
+    MORE_DIMENSIONS_API std::unique_ptr<ChunkSource>
     _wrapStorageForVersionCompatibility(std::unique_ptr<ChunkSource> cs, ::StorageVersion ver) override;
 
-    mce::Color getBrightnessDependentFogColor(mce::Color const& color, float brightness) const override;
+    MORE_DIMENSIONS_API mce::Color getBrightnessDependentFogColor(mce::Color const& color, float brightness) const override;
 
-    short getCloudHeight() const override;
+    MORE_DIMENSIONS_API short getCloudHeight() const override;
 
-    bool hasPrecipitationFog() const override;
+    MORE_DIMENSIONS_API bool hasPrecipitationFog() const override;
 };
 } // namespace more_dimensions
